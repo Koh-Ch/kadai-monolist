80d9b03874fbfd505eb4cc58d0588c7f51ff1d50
koh <ec2-user@ip-172-31-1-214.us-east-2.compute.internal>
2018-03-11 06:48:31 +0000

    user registration

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id]) #temp: 登録確認 @userにブラウザインフォのidを取得して検索代入
+  end
+
+  def new #新規登録用画面へ
+    @user = User.new #中の情報はあとで new.html.erbで代入
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
