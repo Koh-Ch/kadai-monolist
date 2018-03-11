baa71d84357a2015c2ff61f060bd21b61ba59742
koh <ec2-user@ip-172-31-1-214.us-east-2.compute.internal>
2018-03-11 15:11:38 +0000

    want

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:show]
   def show
     @user = User.find(params[:id]) #temp: 登録確認 @userにブラウザインフォのidを取得して検索代入
+    @items = @user.items.uniq
+    @count_want = @user.want_items.count
   end
 
   def new #新規登録用画面へ
