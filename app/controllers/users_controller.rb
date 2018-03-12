d54b3f5213690b69caeb29dc78c369c089ba5cfa
koh <ec2-user@ip-172-31-1-214.us-east-2.compute.internal>
2018-03-12 05:34:18 +0000

    have

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id]) #temp: 登録確認 @userにブラウザインフォのidを取得して検索代入
     @items = @user.items.uniq
     @count_want = @user.want_items.count
+    @count_have = @user.have_items.count
   end
 
   def new #新規登録用画面へ
@@ -15,6 +16,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
+      session[:user_id] = @user.id  ##これでサインアップ後もログイン状態となる
       redirect_to @user
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
